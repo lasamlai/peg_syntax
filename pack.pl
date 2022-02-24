373c74e5da9dfe544634ca0478e3156c5ebe7b64
samlai <samuelli@op.pl>
2022-02-24 23:15:04 +0100

    Version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(peg_syntax).
 title('PEG syntax for prolog').
-version('0.3').
+version('1.0').
 author(samlai, 'piortlierr@gmail.com').
 maintainer(samlai, 'piortlierr@gmail.com').
 packager(samlai, 'piortlierr@gmail.com').
